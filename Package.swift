0db01a5f2e4162c102ff8a89586902eceb30a24d
DrAma999 <andrea.alessandro@gmail.com>
2021-10-15 13:30:19 +0200

    Added supported platforms

--- Package.swift
+++ Package.swift
@@ -3,8 +3,21 @@
 
 import PackageDescription
 
+#if os(Linux)
+let supportedPlatforms: [SupportedPlatform]? = nil
+#else
+let supportedPlatforms: [SupportedPlatform]? = [
+    // Add support for all platforms starting from a specific version.
+    .macOS(.v10_12),
+    .iOS(.v10),
+    .watchOS(.v3),
+    .tvOS(.v10)
+]
+#endif
+
 let package = Package(
     name: "SwiftLinuxBLE",
+    platforms: supportedPlatforms,
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
