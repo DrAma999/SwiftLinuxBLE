0db837488f0a94af96a7367e85b9aefa8f56a6ec
Andrea <andrea.alessandro@gmail.com>
2022-11-09 21:45:21 +0100

    Added clion config file and package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-//#if os(Linux)
-//let supportedPlatforms: [SupportedPlatform]? = nil
-//#else
-//let supportedPlatforms: [SupportedPlatform]? = [
-//    // Add support for all platforms starting from a specific version.
-//    .macOS(.v10_12),
-//    .iOS(.v10),
-//    .watchOS(.v3),
-//    .tvOS(.v10)
-//]
-//#endif
+#if os(Linux)
+let supportedPlatforms: [SupportedPlatform]? = nil
+#else
+let supportedPlatforms: [SupportedPlatform]? = [
+    // Add support for all platforms starting from a specific version.
+    .macOS(.v12),
+    .iOS(.v15),
+    .watchOS(.v8),
+    .tvOS(.v15)
+]
+#endif
 
 let package = Package(
     name: "SwiftLinuxBLE",
-//    platforms: supportedPlatforms,
+    platforms: supportedPlatforms,
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -26,16 +26,15 @@ let package = Package(
             targets: ["SwiftLinuxBLE"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/DrAma999/GATT", .branch("master")),
-        .package(url: "https://github.com/DrAma999/BluetoothLinux", .branch("master")),
-               .package(url: "https://github.com/wickwirew/Runtime", .upToNextMajor(from: "2.1.0"))
+        .package(url: "https://github.com/PureSwift/GATT.git", .upToNextMinor(from: "3.0.4")),
+        .package(url: "https://github.com/PureSwift/BluetoothLinux", .upToNextMinor(from: "5.0.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftLinuxBLE",
-            dependencies: ["GATT", "BluetoothLinux", "Runtime"]),
+            dependencies: ["GATT", "BluetoothLinux"]),
         .testTarget(
             name: "SwiftLinuxBLETests",
             dependencies: ["SwiftLinuxBLE"]),
